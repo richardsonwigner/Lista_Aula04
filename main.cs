91abc36061b843cc625ab81f7db7889e7a4ed723
Richardson Moreira <richardm@ucl.br>
2020-08-26 02:26:15 +0000

    .

--- main.cs
+++ main.cs
@@ -1,7 +1,39 @@
 using System;
 
 class MainClass {
+ 
+ 
+  public static int Conta (int n1,int n2,char op)
+  {
+    int N = 0 ;
+    switch (op)
+    {
+      case '+' :
+        N = n1 + n2;
+        break;
+      case '-' :
+        N = n1 - n2 ;
+        break;
+      case '*' :
+        N = n1 * n2 ;
+        break;
+      case '/' :
+        N = n1 / n2 ;
+        break;
+    }
+   return N;
+  }
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    int n1,n2;
+    char op;
+    Console.WriteLine("Digite o primeiro numero inteiro");
+      n1  = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o segundo numero inteiro");
+      n2  = int.Parse(Console.ReadLine());
+    Console.WriteLine("Escolha a operaçao");
+      op = char.Parse(Console.ReadLine());
+
+    double Total = Conta(n1,n2,op);
+      Console.WriteLine("Sua conta é {0}",Total);
   }
 }
